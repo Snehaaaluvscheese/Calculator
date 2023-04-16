10160e55a5a8587365ff61828447e0df3d6da36a
Snehaaaluvscheese <snehakrishnani255@gmail.com>
2023-04-16 19:20:33 +0530

    Add buttons and display

--- script.js
+++ script.js
@@ -20,12 +20,80 @@ let secondNum;
 
 function operate(firstNum, operator, secondNum) {
     if (operator === "+") {
-        add(firstNum, secondNum);
+       return add(firstNum, secondNum);
     } else if (operator === "-") {
-        subtract(firstNum, secondNum);
+        return subtract(firstNum, secondNum);
     } else if (operator === "*") {
-        multiply (firstNum, secondNum);
+       return  multiply (firstNum, secondNum);
     } else if (operator === "/") {
-        divide (firstNum, secondNum);
+       return divide (firstNum, secondNum);
     }
 }
+
+const inputBox = document.querySelector('#display');
+
+const calcBox1 = document.querySelector('#container1');
+const button7 = document.querySelector('#button7');
+calcBox.appendChild(button7);
+
+const button8 = document.querySelector('#button8');
+calcBox.appendChild(button8);
+
+const button9 = document.querySelector('#button9');
+calcBox.appendChild(button9);
+
+const division = document.querySelector('#divide');
+calcBox.appendChild(division);
+
+const clear = document.querySelector('#clear');
+calcBox.appendChild(clear);
+
+const button4 = document.querySelector('#button4');
+
+
+const button5 = document.querySelector('#button5');
+
+
+const calcBox2 = document.querySelector('#container2');
+
+
+const button6 = document.querySelector('#button6');
+
+const multiplication = document.querySelector('#multiply');
+
+calcBox2.appendChild(button4);
+calcBox2.appendChild(button5);
+calcBox2.appendChild(button6);
+calcBox2.appendChild(multiplication);
+
+const calcBox3 = document.querySelector('#container3');
+
+const button1 = document.querySelector('#button1');
+
+calcBox3.appendChild(button1);
+
+const button2 = document.querySelector('#button2');
+
+calcBox3.appendChild(button2);
+
+const button3 = document.querySelector('#button3');
+
+calcBox3.appendChild(button3);
+
+const subtraction = document.querySelector('#subtract');
+
+calcBox3.appendChild(subtraction);
+
+const calcBox4 = document.querySelector('#container4');
+
+const zero = document.querySelector('#zero');
+calcBox4.appendChild(zero);
+
+const point = document.querySelector('#decimal');
+calcBox4.appendChild(point);
+
+const addition = document.querySelector('#add');
+calcBox4.appendChild(addition);
+
+const equal = document.querySelector('#equals');
+calcBox4.appendChild(equal);
