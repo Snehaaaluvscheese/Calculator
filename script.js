be987f0719b8e54079f9b351f025ff52dfacc964
Snehaaaluvscheese <snehakrishnani255@gmail.com>
2023-04-16 16:56:07 +0530

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b
 }
+
+let firstNum;
+let operator;
+let secondNum;
+
+function operate(firstNum, operator, secondNum) {
+    if (operator === "+") {
+        add(firstNum, secondNum);
+    } else if (operator === "-") {
+        subtract(firstNum, secondNum);
+    } else if (operator === "*") {
+        multiply (firstNum, secondNum);
+    } else if (operator === "/") {
+        divide (firstNum, secondNum);
+    }
+}
