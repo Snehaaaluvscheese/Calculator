5a423d0dce4f5d52299eb13074dae5a62918bba1
Snehaaaluvscheese <snehakrishnani255@gmail.com>
2023-04-18 08:29:04 +0530

    Add buttons 9, / and clear with event listeners

--- script.js
+++ script.js
@@ -92,5 +92,82 @@ const calcBox = document.querySelector('#container');
      });
      // add event listener to button 8 that puts 8 in display
 
+     const button9 = document.createElement('button');
+     // created third button '9' of calculator
+
+     button9.textContent = '9';
+     // added text content to button9
+
+     button9.setAttribute('id', 'button9');
+     // added id to button9
+
+     calcBox.appendChild(button9);
+     // append childNode button 9 to calculator container parentNode
+
+     button9.addEventListener('click', () => {
+        document.getElementById("display").innerHTML = document.getElementById("display").innerHTML + "9";
+        displayValue = "9";
+     });
+     // add event listener to button 9 that puts 9 in display
+
+     const division = document.createElement('button');
+     // created fourth button 'division' of calculator
+
+     division.textContent = '/';
+     // added text content to division
+
+     division.setAttribute('id', 'division');
+     // added id to division
+
+     calcBox.appendChild(division);
+     // append childNode division to calculator container parentNode
+
+     division.addEventListener('click', () => {
+        document.getElementById("display").innerHTML = document.getElementById("display").innerHTML + "/";
+        displayValue = "/";
+     });
+     // add event listener to division that puts / in display
+
+     const clearAll = document.createElement('button');
+     // created fifth button 'clear' of calculator
+
+     clearAll.textContent = 'clear';
+     // added text content to clear
+
+     clearAll.setAttribute('id', 'clearAll');
+     // added id to clearAll
+
+     calcBox.appendChild(clearAll);
+     // append childNode clearAll to calculator container parentNode
+
+     clearAll.addEventListener('click', () => {
+        document.getElementById("display").innerHTML = "";
+        displayValue = "";
+     });
+     // add event listener to clearAll that clears display
+
+     const button4 = document.createElement('button');
+     // created sixth button '4' of calculator
+
+     button4.textContent = '4';
+     // added text content to button4
+
+     button4.setAttribute('id', 'button4');
+     // added id to button4
+
+     calcBox.appendChild(button4);
+     // append childNode button 4 to calculator container parentNode
+
+     button4.addEventListener('click', () => {
+        document.getElementById("display").innerHTML = document.getElementById("display").innerHTML + "4";
+        displayValue = "4";
+     });
+     // add event listener to button 4 that puts 4 in display
+
+     
+
+
+
+
 
 
